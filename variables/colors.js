199e77e63366d0d98846cf35eb44b73b27588a14
Pramod Singh <spramodgusain@gmail.com>
2023-12-07 23:21:01 +0530

    chore: create design calculator (#1)

--- variables/colors.js
+++ variables/colors.js
@@ -6,13 +6,12 @@ module.exports = {
     'transparent': 'transparent',
 
     /* Defult Color Variables */
-    'yellow-color': '#E8B835',
-    'primary-color': '#03658C',
-    'secondary-color': '#F24405',
-    'success-color': '#87340E',
-    'info-color': '#0D0D0D',
-    'warning-color': '#F2E4C9',
-    'danger-color': '#f3f3f3',
+    'primary-color': '#FF4500',
+    'secondary-color': '#EAECF0',
+    'success-color': '#5C5C5C',
+    'info-color': '#6C6C6C',
+    'warning-color': '#ACACAC',
+    'danger-color': '#',
 
     /* Alternate Color Variables */
     // 'alt-color': '#222',
